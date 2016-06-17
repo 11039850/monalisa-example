498c83c601341b78eff7939e65977a8597afe26b
zzg <11039850@qq.com>
2016-06-17 15:33:11 +0800

    style

--- src/main/java/test/action/UserAction.java
+++ src/main/java/test/action/UserAction.java
@@ -1,10 +1,16 @@
 package test.action;
 
+import test.TestDB;
 import test.dao.UserBlogDao;
 import test.result.UserBlogs;
 import test.testdb.User;
 
+import com.tsc9526.monalisa.core.query.DataMap;
+import com.tsc9526.monalisa.core.query.Page;
 import com.tsc9526.monalisa.core.query.Query;
+import com.tsc9526.monalisa.core.query.Tx;
+import com.tsc9526.monalisa.core.query.datatable.DataTable;
+import com.tsc9526.monalisa.core.query.model.Record;
 
 /**
  * 调用示例
@@ -13,29 +19,122 @@ public class UserAction {
 	public static void main(String[] args)throws Exception {
 		UserAction action=new UserAction();
 		
-		action.saveLoad();
-		action.findUserBlogs(1);
+		action.testDAO();
+		
+		action.testDBAction();
+		
+		action.testDynamic();
 	}
+	 
 	
-	/**
-	 * 这里需要调用 Query.Create()来创建数据访问类. 不能直接调用new, 否则会丢失@Tx标记方法的事务特性， 以及数据访问类更新加载的特性
-	 */
+	//这里需要调用 Query.Create()来创建数据访问类. 不能直接调用new, 否则会丢失@Tx标记方法的事务特性， 以及数据访问类更新加载的特性
 	private UserBlogDao dao=Query.create(UserBlogDao.class);
-	
-	public void findUserBlogs(int user_id){
-		for(UserBlogs x: dao.selectUserBlogs(user_id)){
+			
+	//调用DAO
+	public void testDAO(){
+		for(UserBlogs x: dao.selectUserBlogs(1)){
 			System.out.println(x.getContent());
 		}
 	}
 	
-	public void saveLoad(){
-		new User().setName("zzg123").setStatus(1).save();
+	//基本数据库操作
+	public void testDBAction(){
+		//insert
+		new User().setName("zzg.zhou").setStatus(1).save();
 		
-		new User(1).load().setName("zzgUpdate").update();
+		//parse data from type: Map, json/xml string, JsonObject(Gson), HttpServletRequest, JavaBean
+		new User().parse("{'name':'oschina','status':0}").save();
+		new User().parse("<data> <name>china01</name><status>1</status> </data>").save();
 		
+		//select
+		User.SELECT().selectByPrimaryKey(1);
+		
+		//SQL: SELECT * FROM `user` WHERE `name` = 'zzg.zhou'
+		User.SELECT().selectOne("name=?", "zzg.zhou");
+		
+		//SQL: SELECT `name`, `status` FROM `user`
+		User.SELECT().include("name","status").select();
+		 
+		Page<User> page=User.WHERE().name.like("zzg%").status.in(1,2,3).SELECT().selectPage(10,0);
+		System.out.println(page.getTotalRow());
+		
+		//SQL: SELECT * FROM `user` WHERE `name` like 'zzg%' AND `status` IN(0, 1)
 		for(User x:User.WHERE().name.like("zzg%").status.in(0, 1).SELECT().select()){
 			System.out.println(x);
 		}
-	}       
+				
+		//SQL: SELECT * FROM `user` WHERE (`name` like 'zzg%' AND `status` >= 0) OR (`name` = 'zzg' AND `status` > 1) ORDER BY `status` ASC 
+		for(User x:User.WHERE()
+					.name.like("zzg%").status.ge(0)
+					.OR()
+					.name.eq("zzg").status.gt(1)
+					.status.asc()
+					.SELECT().select()){ //SELECT / delete / update
+			System.out.println(x);
+		}
+		
+	 	 
+		//general query
+		TestDB.DB.select("SELECT * FROM user WHERE name like ?","zzg%");
+		TestDB.DB.createQuery().add("SELECT * FROM user WHERE name like ?","zzg%").getList(User.class);
+		 
+		Query q=new Query(TestDB.DB);
+		DataTable<DataMap> rs=q.add("SELECT * FROM user WHERE name like ?","zzg%")
+		 .add(" AND status ").in(1,2,3)
+		 .getList();
+		for(User x:rs.as(User.class)){
+			System.out.println(x);
+		}
+		
+		//update
+		User user=User.SELECT().selectOne("name=?", "zzg.zhou");
+		user.setStatus(3).update();
+		
+		User updateTo=new User().setName("tsc9526");
+		User.WHERE().name.like("zzg%").update(updateTo);
+	
+		
+		//transaction
+		Tx.execute(new Tx.Atom() {
+			public int execute() {
+				new User().setName("name001").setStatus(1).save();
+				new User().setName("name002").setStatus(2).save();
+				//... other database operation
+				return 0;
+			}
+		});
+		 
+		
+		//delete
+		user.delete();
+
+		//SQL: DELETE FROM `user` WHERE `name`='china01'
+		User.WHERE().name.eq("china01").delete();
+		
+		//User.DELETE().deleteAll();
+	}  
+	 
+	
+	//使用动态模型
+	public void testDynamic(){
+		Record r=new Record("user").use(TestDB.DB);
+		r.set("name", "jjyy").set("status",1)
+		 .save();
+		
+		//SQL: SELECT * FROM `user` WHERE (`name` like 'jjyy%' AND `status` >= 0) OR (`name` = 'zzg' AND `status` > 1) ORDER BY `status` ASC 
+		for(Record x:r.WHERE()
+				.field("name").like("jjyy%").field("status").ge(0)
+				.OR()
+				.field("name").eq("zzg").field("status").gt(1)
+				.field("status").asc()
+				.SELECT().select()){
+			System.out.println(x);
+		} 
+		
+		//SQL: DELETE FROM `user` WHERE `name` like 'jjyy%' AND `status` >= 0
+		r.WHERE()
+		 .field("name").like("jjyy%").field("status").ge(0)
+		 .delete();
+	}
 
 }
