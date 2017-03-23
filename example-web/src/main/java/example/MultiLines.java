5670b20c5c5f0857ee40a827f705fadd39f844a3
zzg <11039850@qq.com>
2017-03-23 15:59:43 +0800

    add multi lines example

---
+++ example-web/src/main/java/example/MultiLines.java
@@ -0,0 +1,14 @@
+package example;
+ 
+public class MultiLines {
+	static String name="zzg";
+	
+	public static void main(String[] args) {
+		System.out.println(""+/**~!{*/""
+			+ "Hello"
+			+ "\r\n	\"" +(name)+ "\"!"
+		+ "\r\n"/**}*/);
+
+	}
+
+}
